11c600bfdee7c5aa4b71a2b1b5522acccf170717
alex <alex@pdp7.net>
2024-07-28 20:54:42 +0200

    Add extract function

--- src/py_pg_embed/__init__.py
+++ src/py_pg_embed/__init__.py
@@ -1,6 +1,8 @@
 import pathlib
 import shutil
+import tarfile
 import urllib.request
+import zipfile
 
 
 def get_url(os="linux", arch="amd64", version="16.3.0"):
@@ -11,3 +13,10 @@ def _download(url, path: pathlib.Path):
     with path.open("wb") as out:
         with urllib.request.urlopen(url) as in_:
             shutil.copyfileobj(in_, out)
+
+
+def extract(package, path: pathlib.Path, os="linux", arch="x86_64"):
+    with zipfile.ZipFile(package) as zip:
+        with zip.open(f"postgres-{os}-{arch}.txz") as txz:
+            with tarfile.open(mode="r:xz", fileobj=txz) as tar:
+                tar.extractall(path)
