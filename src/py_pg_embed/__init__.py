e28ea195e1b69d95732d1455d77ca2ce418200c2
alex <alex@pdp7.net>
2024-07-28 21:07:31 +0200

    Add command runner, example

--- src/py_pg_embed/__init__.py
+++ src/py_pg_embed/__init__.py
@@ -1,8 +1,26 @@
 import pathlib
 import shutil
+import subprocess
 import tarfile
 import urllib.request
 import zipfile
+
+"""
+import pathlib, py_pg_embed
+
+jar = pathlib.Path("tmp.jar")
+pg = pathlib.Path("pg")
+data = pathlib.Path("data")
+
+py_pg_embed._download(py_pg_embed.get_url(), jar)
+py_pg_embed.extract(jar, pg)
+py_pg_embed.initdb(pg, data)
+py_pg_embed.postgres(pg,data)
+
+...
+
+$ psql -h localhost postgres
+"""
 
 
 def get_url(os="linux", arch="amd64", version="16.3.0"):
@@ -20,3 +38,11 @@ def extract(package, path: pathlib.Path, os="linux", arch="x86_64"):
         with zip.open(f"postgres-{os}-{arch}.txz") as txz:
             with tarfile.open(mode="r:xz", fileobj=txz) as tar:
                 tar.extractall(path)
+
+
+def initdb(extracted: pathlib.Path, datadir: pathlib.Path):
+    subprocess.run([extracted / "bin" / "initdb", "-D", datadir], check=True)
+
+
+def postgres(extracted: pathlib.Path, datadir: pathlib.Path):
+    subprocess.run([extracted / "bin" / "postgres", "-D", datadir], check=True)
